4ff25fc4e0dae64beecf9336dd19547e5dcdb950
BlackShadowBug <ale_95_marce@hotmail.com>
2024-03-11 20:03:50 -0300

    Front

--- src/components/GameDetail/GameDetail.jsx
+++ src/components/GameDetail/GameDetail.jsx
@@ -5,112 +5,124 @@ import axios from "axios";
 import styles from "./GameDetail.module.css";
 
 function GameDetail() {
- const { id } = useParams();
- const [game, setGame] = useState();
- const navigate = useNavigate();
+  const { id } = useParams();
+  const [game, setGame] = useState();
+  const navigate = useNavigate();
 
- useEffect(() => {
-  const fetchGame = async () => {
-   const response = await axios.get(
-    `https://back-1ngv.onrender.com/videogames/id/${id}`
-   );
-   setGame(response.data);
+  useEffect(() => {
+    const fetchGame = async () => {
+      const response = await axios.get(
+        `http://localhost:3001/videogames/id/${id}`
+      );
+      setGame(response.data);
+    };
+    fetchGame();
+  }, [id]);
+
+  const handleClick = () => {
+    navigate("/home");
   };
-  fetchGame();
- }, [id]);
 
- const handleClick = () => {
-  navigate("/home");
- };
+  const handleDelete = async () => {
+    const userCode = prompt(
+      "Por favor, ingresa el código para eliminar el juego:"
+    );
+    const yourCode = "1234"; 
 
- const handleDelete = async () => {
-  try {
-   await axios.delete(`https://back-1ngv.onrender.com/videogames/delete/${id}`);
-   alert("Videojuego eliminado exitosamente");
-   navigate("/home");
-  } catch (error) {
-   console.error(error);
-   alert("Hubo un error al intentar eliminar el videojuego");
-  }
- };
+    if (userCode === yourCode) {
+      try {
+        await axios.delete(`http://localhost:3001/videogames/delete/${id}`);
+        alert("Videojuego eliminado exitosamente");
+        navigate("/home");
+      } catch (error) {
+        console.error(error);
+        alert("Hubo un error al intentar eliminar el videojuego");
+      }
+    } else {
+      alert(
+        "El código ingresado es incorrecto. El videojuego no se ha eliminado."
+      );
+    }
+  };
 
- return (
-  <div className={styles.background}>
-   {game ? (
-    <>
-     {game.isFromDatabase && (
-      <button className={styles.button} onClick={handleDelete}>
-       Eliminar
-      </button>
-     )}
+  return (
+    <div className={styles.background}>
+      {game ? (
+        <>
+          {game.isFromDatabase && (
+            <button className={styles.button} onClick={handleDelete}>
+              Eliminar
+            </button>
+          )}
 
-     <button className={styles.button} onClick={handleClick}>
-      Home
-     </button>
+          <button className={styles.button} onClick={handleClick}>
+            Home
+          </button>
 
-     <div>
-      <section>
-       <label className={styles.label}>
-        <span className={styles.underline}>ID</span>:{" "}
-       </label>
-       <h1 className={styles.h1}>{game.id}</h1>
-      </section>
+          <div>
+            <section>
+              <label className={styles.label}>
+                <span className={styles.underline}>ID</span>:{" "}
+              </label>
+              <h1 className={styles.h1}>{game.id}</h1>
+            </section>
 
-      <section>
-       <h2 className={styles.h2}>
-        <span className={styles.underline}>NAME</span>: {game.name}
-       </h2>
-      </section>
+            <section>
+              <h2 className={styles.h2}>
+                <span className={styles.underline}>NAME</span>: {game.name}
+              </h2>
+            </section>
 
-      <section>
-       <img
-        className={styles.img}
-        src={game.background_image}
-        alt={game.name}
-       />
-      </section>
+            <section>
+              <img
+                className={styles.img}
+                src={game.background_image}
+                alt={game.name}
+              />
+            </section>
 
-      <section>
-       <p className={styles.p}>
-        <span className={styles.underline}>PLATFORMS</span>: {game.platforms}
-       </p>
-      </section>
+            <section>
+              <p className={styles.p}>
+                <span className={styles.underline}>PLATFORMS</span>:{" "}
+                {game.platforms}
+              </p>
+            </section>
 
-      <section>
-       <p className={styles.p}>
-        <span className={styles.underline}>DESCRIPTION</span>:{" "}
-        {game.description_raw}
-       </p>
-      </section>
+            <section>
+              <p className={styles.p}>
+                <span className={styles.underline}>DESCRIPTION</span>:{" "}
+                {game.description_raw}
+              </p>
+            </section>
 
-      <section>
-       <p className={styles.p}>
-        <span className={styles.underline}>GENRE</span>: {game.genres}
-       </p>
-      </section>
+            <section>
+              <p className={styles.p}>
+                <span className={styles.underline}>GENRE</span>: {game.genres}
+              </p>
+            </section>
 
-      <section>
-       <p className={styles.p}>
-        <span className={styles.underline}>RATING</span>: {game.rating}
-       </p>
-      </section>
-     </div>
-    </>
-   ) : (
-    <div className={styles.loaderContainer}>
-     <div className={styles.loader}>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-      <div className={styles.loaderSquare}></div>
-     </div>
+            <section>
+              <p className={styles.p}>
+                <span className={styles.underline}>RATING</span>: {game.rating}
+              </p>
+            </section>
+          </div>
+        </>
+      ) : (
+        <div className={styles.loaderContainer}>
+          <div className={styles.loader}>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+            <div className={styles.loaderSquare}></div>
+          </div>
+        </div>
+      )}
     </div>
-   )}
-  </div>
- );
+  );
 }
 
 export default GameDetail;
